4d9df3f5bf9b5f297400773b9169c322c0ce0ea6
eka-git <770408554@qq.com>
2021-03-28 21:14:49 +0800

    第三次

---
+++ c1/c1/GenericList.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace c1
+{
+    public class GenericList<T>
+    {
+        private Node<T> head;
+        private Node<T> tail;
+
+        public GenericList()
+        {
+            head = null;
+            tail = null;
+        }
+
+        public Node<T> Head
+        {
+            get => head; 
+        }
+
+        public void Add(T t)
+        {
+            Node<T> n = new Node<T>(t);
+            if (tail == null)
+            {
+                head = tail = n;
+            }
+            else
+            {
+                tail.Next = n;
+                tail = n;
+            }
+
+        }
+
+        public void ForEach(Action<T> action)
+        {
+            for (Node<T> m = head; m.Next != null; m = m.Next)
+            {
+                action(m.Data);
+            }
+        }
+    }
+}
---
+++ c2/c2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace c2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Clock clock = new Clock();
+            DateTime atime = new DateTime();
+            atime = DateTime.Now.AddSeconds(2);
+            clock.SetAlarmTime(atime);
+            clock.Start();
+        }
+    }
+}
