e56418c3c038abf96d091cf08a9d7de0e6a5f9cf
eka-git <770408554@qq.com>
2021-04-10 14:24:56 +0800

    第四次作业

---
+++ d1/d1/IOrderService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace d1
+{
+    interface IOrderService
+    {
+        void addOrder();
+        void removeOrder();
+        void searchOrder(int i);
+  //      void export();
+  //      void import();
+    }
+}
